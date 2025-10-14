7d23d6115a327f6c4e72cb5c28010e27d6ba8e67
mass-inbound <development@massinbound.com>
2025-10-14 16:52:03 -0400

    gtm id updated root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -156,7 +156,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-<YOUR_GTM_ID>');`,
+            })(window,document,'script','dataLayer','GTM-KS2ZHPMR');`,
          }}
         ></Script>
          {/* --- PASTE META PIXEL CODE START --- */}
@@ -202,7 +202,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <noscript>
           <iframe
             title="Google Tag Manager"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-<YOUR_GTM_ID>"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KS2ZHPMR"
             height="0"
             width="0"
             style={{

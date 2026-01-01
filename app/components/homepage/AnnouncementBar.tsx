16acb9568684d9cfd224bb3518a534b5e615617b
mass-inbound <development@massinbound.com>
2026-01-01 09:31:30 -0500

    50% off enable

--- app/components/homepage/AnnouncementBar.tsx
+++ app/components/homepage/AnnouncementBar.tsx
@@ -13,7 +13,7 @@ interface AnnouncementBarContentProps {
 }
 
 function AnnouncementBarContent({
-  message = "Save 30% Off Sitewide!",
+  message = "New Year Savings - 50% Off Sitewide!",
   cart
 }: AnnouncementBarContentProps) {
 
@@ -47,7 +47,7 @@ function AnnouncementBarContent({
 
 
 export function AnnouncementBar({
-  message = "Save 30% Off Sitewide!",
+  message = "New Year Savings - 50% Off Sitewide!",
   cart
 }: AnnouncementBarProps) {
   if (!cart) {
